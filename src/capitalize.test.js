2745e461164ce6823f67a13a422f82b1aa540180
Kaitlin Swanson <kateswanson94@gmail.com>
2021-07-05 19:16:50 -0700

    test added

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -3,4 +3,4 @@ import { capitalizeFirst } from './capitalize.js';
 
 test('properly capitalize first letter of a string', () => {
     expect(capitalizeFirst('anything')).toMatch('Anything');
-});
+}); 
